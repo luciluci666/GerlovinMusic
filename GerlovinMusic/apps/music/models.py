15b0204d31328f794f00bb02526b9c3ded181721
luciluci666 <gerroma01@gmail.com>
2022-04-05 18:47:23 +0300

    add playlists

--- GerlovinMusic/apps/music/models.py
+++ GerlovinMusic/apps/music/models.py
@@ -5,3 +5,18 @@ class Music(models.Model):
     desc = models.CharField('Описание', max_length=250, null=True, blank=True)
     audio_file = models.FileField('Загрузить песню', blank=True, null=True, upload_to='media/music')
     pub_date = models.DateField()
+
+class Playlist(models.Model):
+    name = models.CharField('Название плэй-листа', max_length=100, null=True, blank=True)
+    first = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="first")
+    second = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="second")
+    third = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="third")
+    fourth = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="fourth")
+    fiveth = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="fiveth")
+    sixth = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="sixth")
+    seventh = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="seventh")
+    eighth = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="eighth")
+    ninth = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="ninth")
+    tenth = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="tenth")
+
+    
