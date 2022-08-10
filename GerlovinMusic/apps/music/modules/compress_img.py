050cb10a210ab92b93099e316d464ba539da4a6a
luciluci666 <gerroma01@gmail.com>
2022-08-10 20:34:33 +0300

    Create compress_img.py

---
+++ GerlovinMusic/apps/music/modules/compress_img.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import os
+
+
+class Compress():
+    def compress_img(self, path, quality=70):
+        img = Image.open(path)
+
+        if img.height > 300 or img.width > 300:
+            img.save(path.path, format="JPEG", quality=quality)
+
+    def folder_imgs_compress(self, folder, quality=70):
+        filelist = []
+
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                #append the file name to the list
+                filelist.append(os.path.join(root,file))
+
+        #print all the file names
+        for path in filelist:
+            if not "png" in path:
+                img = Image.open(path)
+                if img.height > 300 or img.width > 300:
+                    img.save(path, format="JPEG", quality=quality)
+        
+
+
+Compress().folder_imgs_compress("GerlovinMusic\static\static_files\img/artist", 50)
