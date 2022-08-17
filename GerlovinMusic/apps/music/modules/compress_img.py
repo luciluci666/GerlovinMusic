15155997c66b9fc71394b2aa6b5e87fdf54bd9d4
luciluci666 <gerroma01@gmail.com>
2022-08-17 22:36:41 +0300

    Update compress_img.py

--- GerlovinMusic/apps/music/modules/compress_img.py
+++ GerlovinMusic/apps/music/modules/compress_img.py
@@ -4,27 +4,53 @@ import os
 
 class Compress():
     def compress_img(self, path, quality=70):
-        img = Image.open(path)
+        if not "png" in path and not "svg" in path and not "mp3" in path:
+                img = Image.open(path)
+                img_size = os.stat(path).st_size / 1024
+                print(f'File Size in Kb is {img_size}')
+                if img_size > 300:
+                    img.save(path, format="JPEG", quality=quality)
+                    print(path + " - compressed!")
 
-        if img.height > 300 or img.width > 300:
-            img.save(path.path, format="JPEG", quality=quality)
 
-    def folder_imgs_compress(self, folder, quality=70):
+    def folder_imgs_compress(self, quality=70, folders=[], folder=""):
         filelist = []
-
-        for root, dirs, files in os.walk(folder):
-            for file in files:
-                #append the file name to the list
-                filelist.append(os.path.join(root,file))
+        if folder == "" and not folders == []:
+            try:
+                for folder in folders:
+                    for root, dirs, files in os.walk(folder):
+                        for file in files:
+                            #append the file name to the list
+                            filelist.append(os.path.join(root,file))
+            except:
+                print("List is invalid!")
+        else:
+            try:
+                for root, dirs, files in os.walk(folder):
+                    for file in files:
+                        #append the file name to the list
+                        filelist.append(os.path.join(root,file))
+            except:
+                print("Folder is invalid!")
 
         #print all the file names
         for path in filelist:
-            if not "png" or not "svg" in path :
+            if not "png" in path and not "svg" in path and not "mp3" in path:
                 img = Image.open(path)
-                if img.height > 300 or img.width > 300:
+                img_size = int(os.stat(path).st_size / 1024)
+                if img_size > 300:
                     img.save(path, format="JPEG", quality=quality)
-                    print(path + " - compressed!")
+                    print(path + " - compressed! File Size is " + str(img_size) + "Kb")
         
 
+
 if __name__ == "__main__":
-    Compress().folder_imgs_compress("GerlovinMusic\static\static_files\img", 20)
+    folders = [
+        "GerlovinMusic\static\static_files\img",
+        "GerlovinMusic\static\img",
+        "GerlovinMusic\static\static_files\media",
+        "GerlovinMusic\static\media",
+    ]
+
+    Compress().folder_imgs_compress(20, folders=folders)
+    
